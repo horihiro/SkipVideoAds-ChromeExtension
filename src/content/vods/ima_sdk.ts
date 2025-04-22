969612e7019b9fce9cedb12cdfd0af78ee6bbb5a
horihiro <hori.kawa.shima.tool+github@gmail.com>
2025-04-22 11:05:56 +0900

    Refactor IMASdk: update ad handling to display a "Skipping Ads..." overlay and adjust time gap condition for skipping

--- src/content/vods/ima_sdk.ts
+++ src/content/vods/ima_sdk.ts
@@ -9,9 +9,20 @@ export class IMASdk extends Vod {
       const videoElms: HTMLMediaElement[] = (Array.from(document.querySelectorAll('video[title="Advertisement"]')) as HTMLMediaElement[]).filter(e => !e.ontimeupdate);
       if (videoElms.length === 0) return;
       videoElms.forEach(videoElm => {
-        videoElm.style.visibility = 'hidden';
+        // videoElm.style.visibility = 'hidden';
+        const telop = document.createElement('div');
+        telop.style.position = 'absolute';
+        telop.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        telop.style.top = '0';
+        telop.style.left = '0';
+        telop.style.width = '100%';
+        telop.style.height = '100%';
+        telop.style.lineHeight = '50';
+        telop.style.textAlign = 'center';
+        telop.textContent = 'Skiping Ads...';
+        videoElm.parentElement?.appendChild(telop);
         const ontimeupdate = (e) => {
-          if (e.target.duration - e.target.currentTime <= this.TIMEGAP) return;
+          if (e.target.duration - e.target.currentTime <= this.TIMEGAP*2) return;
           const video = (e.target as HTMLMediaElement);
           if (isNaN(video.duration) || isNaN(video.currentTime) || video.currentTime <= 0 || video.duration <= 0) return;
           const skipTime = video.duration - video.currentTime;
