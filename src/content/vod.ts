84dab1c1d0bc23d63eaa903d936257bbfd2239d8
horihiro <hori.kawa.shima.tool+github@gmail.com>
2025-04-23 21:46:34 +0900

    Refactor Vod class: update telop SVG for ad skipping indication with new animation and styling

--- src/content/vod.ts
+++ src/content/vod.ts
@@ -12,7 +12,17 @@ export class Vod {
     this.telop.style.width = '100%';
     this.telop.style.height = '100%';
     this.telop.style.color = 'white';
-    this.telop.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="100%" height="100%"><rect fill="#0078D4" stroke="#0078D4" stroke-width="8" width="30" height="30" x="25" y="85"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></rect><rect fill="#0078D4" stroke="#0078D4" stroke-width="8" width="30" height="30" x="85" y="85"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></rect><rect fill="#0078D4" stroke="#0078D4" stroke-width="8" width="30" height="30" x="145" y="85"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></rect></svg>'
+    this.telop.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 50">
+  <polygon points="32,18 45,25 32,32" fill="white">
+    <animate attributeName="opacity" from="1" to="0" dur="0.5s" repeatCount="indefinite"></animate>
+  </polygon>
+  <polygon points="45,18 58,25 45,32" fill="white">
+    <animate attributeName="opacity" from="1" to="0" dur="0.5s" repeatCount="indefinite"></animate>
+  </polygon>
+  <rect x="58" y="18" width="3" height="14" fill="white">
+    <animate attributeName="opacity" from="1" to="0" dur="0.5s" repeatCount="indefinite"></animate>
+  </rect>
+</svg>`
   }
   startWatching() {
     console.error('Need to implement startWatching');
