c862e6af7ff68bfd057df46fea598229c39020fe
horihiro <hori.kawa.shima.tool+github@gmail.com>
2025-04-23 21:08:11 +0900

    Refactor Vod class: update telop element initialization to use SVG for ad skipping indication and remove unnecessary styles

--- src/content/vod.ts
+++ src/content/vod.ts
@@ -4,17 +4,15 @@ export class Vod {
   constructor() {
     this.TIMEGAP = 0.1;
     this.telop = document.createElement('div');
+    this.telop.className = 'skip-vod-telop';
     this.telop.style.position = 'absolute';
     this.telop.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
     this.telop.style.top = '0';
     this.telop.style.left = '0';
     this.telop.style.width = '100%';
     this.telop.style.height = '100%';
-    this.telop.style.lineHeight = '50';
     this.telop.style.color = 'white';
-    this.telop.style.textAlign = 'center';
-    this.telop.textContent = 'Skiping Ads...';
-
+    this.telop.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="100%" height="100%"><rect fill="#0078D4" stroke="#0078D4" stroke-width="8" width="30" height="30" x="25" y="85"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></rect><rect fill="#0078D4" stroke="#0078D4" stroke-width="8" width="30" height="30" x="85" y="85"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></rect><rect fill="#0078D4" stroke="#0078D4" stroke-width="8" width="30" height="30" x="145" y="85"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></rect></svg>'
   }
   startWatching() {
     console.error('Need to implement startWatching');
