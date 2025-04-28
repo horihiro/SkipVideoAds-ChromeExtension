2d2c91dd6b1a80018923b9c822381626e21ea7e9
horihiro <hori.kawa.shima.tool+github@gmail.com>
2025-04-28 18:26:45 +0900

    refactor: streamline skip mode loading and handling in main.ts for improved clarity and functionality

--- src/content/main.ts
+++ src/content/main.ts
@@ -2,11 +2,35 @@ import { SkipMode, Vod } from './vod';
 import { AmazonPrimeVideo } from './vods/amazon_prime_video';
 import { YouTube } from './vods/youtube';
 import { IMASdk } from './vods/ima_sdk';
+import { minimatch } from 'minimatch';
 
 (() => {
-  const defaultSkipMode:SkipMode = SkipMode.auto;
+  const DEFAULT_SKIP_MODE: SkipMode = SkipMode.auto;
+  const DEFAULT_SKIP_RULE = { priority: 65535, pattern: 'https://*/**', skipMode: DEFAULT_SKIP_MODE };
+
+  const loadSkipMode = async (href: string): Promise<SkipMode> => {
+    const skipRules = (await chrome.storage.local.get(['skipRules'])).skipRules;
+    if (!skipRules || skipRules.length === 0) {
+      await chrome.storage.local.set({ 'skipRules': [DEFAULT_SKIP_RULE] });
+      return DEFAULT_SKIP_MODE;
+    }
+    const skipMode = skipRules.sort((r1, r2) => r1.priority > r2.priority ? 1 : -1).reduce((prev, skipRule) => {
+      return prev || (minimatch(href, skipRule.pattern) ? skipRule.skipMode : null);
+    }, null);
+    console.debug('skipMode:', skipMode);
+    return skipMode || DEFAULT_SKIP_MODE;
+  };
 
   window.addEventListener('load', async () => {
+    let vod:Vod = null;
+    chrome.storage.onChanged.addListener(async (_, area) => {
+      if (area !== 'local') return;
+
+      const skipMode = await loadSkipMode(location.href);
+      if (skipMode === SkipMode.none) return;
+      vod.startWatching(skipMode || DEFAULT_SKIP_MODE);
+    });
+
     let observer: MutationObserver | null = null;
     const init = (href) => {
       switch (true) {
@@ -28,9 +52,9 @@ import { IMASdk } from './vods/ima_sdk';
 
             observer.disconnect();
             console.debug('IMA SDK detected');
-            const vod = new IMASdk();
-            const { skipMode } = (await chrome.storage.local.get(['skipMode']));
-            vod.startWatching(skipMode || defaultSkipMode);
+            vod = new IMASdk();
+            const skipMode = await loadSkipMode(location.href);
+            vod.startWatching(skipMode || DEFAULT_SKIP_MODE);
           });
           observer.observe(document.body, {
             childList: true,
@@ -40,17 +64,10 @@ import { IMASdk } from './vods/ima_sdk';
           return null;
       }
     };
-    const vod: Vod = init(location.href);
+    vod = init(location.href);
     if (!vod) return;
 
-    const { skipMode } = (await chrome.storage.local.get(['skipMode']));
-    vod.startWatching(skipMode || defaultSkipMode);
-
-    chrome.storage.onChanged.addListener(async (changes, area) => {
-      if (area !== 'local') return;
-
-      const { skipMode } = (await chrome.storage.local.get(['skipMode']));
-      vod.startWatching(skipMode || defaultSkipMode);
-    });
+    const skipMode = await loadSkipMode(location.href);
+    vod.startWatching(skipMode || DEFAULT_SKIP_MODE);
   });
 })();
